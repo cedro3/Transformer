dba564bdd50ea6a59040e5eda5fbd38897ec2cb7
cedro3 <jun40vn@gmail.com>
2020-07-26 21:27:45 +0900

    simple

--- dataloader.py
+++ dataloader.py
@@ -15,59 +15,6 @@ from torchtext.vocab import Vectors
 def get_IMDb_DataLoaders_and_TEXT(max_length=256, batch_size=24):
     """IMDbのDataLoaderとTEXTオブジェクトを取得する。 """
 
-    # 訓練データのtsvファイルを作成します
-    f = open('./data/IMDb_train.tsv', 'w')
-
-    path = './data/aclImdb/train/pos/'
-    for fname in glob.glob(os.path.join(path, '*.txt')):
-        with io.open(fname, 'r', encoding="utf-8") as ff:
-            text = ff.readline()
-
-            # タブがあれば消しておきます
-            text = text.replace('\t', " ")
-
-            text = text+'\t'+'1'+'\t'+'\n'
-            f.write(text)
-
-    path = './data/aclImdb/train/neg/'
-    for fname in glob.glob(os.path.join(path, '*.txt')):
-        with io.open(fname, 'r', encoding="utf-8") as ff:
-            text = ff.readline()
-
-            # タブがあれば消しておきます
-            text = text.replace('\t', " ")
-
-            text = text+'\t'+'0'+'\t'+'\n'
-            f.write(text)
-
-    f.close()
-
-   # テストデータの作成
-    f = open('./data/IMDb_test.tsv', 'w')
-
-    path = './data/aclImdb/test/pos/'
-    for fname in glob.glob(os.path.join(path, '*.txt')):
-        with io.open(fname, 'r', encoding="utf-8") as ff:
-            text = ff.readline()
-
-            # タブがあれば消しておきます
-            text = text.replace('\t', " ")
-
-            text = text+'\t'+'1'+'\t'+'\n'
-            f.write(text)
-
-    path = './data/aclImdb/test/neg/'
-    for fname in glob.glob(os.path.join(path, '*.txt')):
-        with io.open(fname, 'r', encoding="utf-8") as ff:
-            text = ff.readline()
-
-            # タブがあれば消しておきます
-            text = text.replace('\t', " ")
-
-            text = text+'\t'+'0'+'\t'+'\n'
-            f.write(text)
-    f.close()
-
     def preprocessing_text(text):
         # 改行コードを消去
         text = re.sub('<br />', '', text)
